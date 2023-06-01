d62d92963d50577d6c2220e17a0f2a8403eb5b13
taemin <xoals3094@naver.com>
2023-06-02 01:34:48 +0900

    fix: 닉네임 문자열 인코딩 방식 수정

--- script.py
+++ script.py
@@ -1,5 +1,6 @@
 import jwt
 import json
+import base64
 
 secret_key = 'dev'
 
@@ -24,8 +25,13 @@ def token_check(environ, start_response):
         response = [json.dumps(body).encode('utf-8')]
         return response
 
+    nickname = payload['nickname']
+    nickname_bytes = nickname.encode('utf-8')
+    nickname_base64 = base64.b64encode(nickname_bytes)
+
     headers.append(('user_id', str(payload['user_id'])))
-    headers.append(('nickname', payload['nickname'].encode('utf-8').decode('iso-8859-1')))
+    headers.append(('nickname', nickname_base64.decode('latin-1')))
+
     status = '200'
     start_response(status, headers)
     response = ['success'.encode('utf-8')]
